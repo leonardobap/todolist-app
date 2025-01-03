d19d7a22482628517c8ea6df044383223c29a6b1
Leonardo <contatoleonardobaptista@gmail.com>
2025-01-02 21:28:23 -0300

    removing component EmptyTask and returning amount of tasks and tasks completed

--- src/components/SummaryTasks/index.tsx
+++ src/components/SummaryTasks/index.tsx
@@ -1,24 +1,35 @@
 import { View, Text } from "react-native";
 
 import { styles } from "./style";
-import { EmptyTask } from "../EmptyTasks";
 
-export function SummaryTasks() {
+interface Task {
+  name: string;
+  isCompleted: boolean;
+  id: string;
+}
+
+interface TasksProps {
+  tasks: Task[];
+}
+
+export function SummaryTasks({ tasks }: TasksProps) {
+  const completedTasks = tasks.filter(tasks => {
+    return tasks.isCompleted
+  })
+
   return (
     <View>
       <View style={styles.summaryTasksContainer}>
         <View style={styles.summaryItem}>
           <Text style={styles.createdTasks}>Criadas</Text>
-          <Text style={styles.counter}>0</Text>
+          <Text style={styles.counter}>{tasks.length}</Text>
         </View>
 
         <View style={styles.summaryItem}>
           <Text style={styles.completedTasks}>Concluídas</Text>
-          <Text style={styles.counter}>0</Text>
+          <Text style={styles.counter}>{completedTasks.length}</Text>
         </View>
       </View>
-
-      {/* <EmptyTask /> */}
     </View>
   );
 }
