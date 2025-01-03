d6331ba8aaef044bd75fd13c61b4574bfd4fea7e
Leonardo <contatoleonardobaptista@gmail.com>
2025-01-02 21:25:43 -0300

    adding options to add and remove a task and returning tasks in FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,63 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { CreateTask } from "../../components/CreateTask";
 import { SummaryTasks } from "../../components/SummaryTasks";
 import { CardTask } from "../../components/CardTask";
+import { useEffect, useState } from "react";
+import { EmptyTask } from "../../components/EmptyTasks";
+
+interface Task {
+  name: string;
+  isCompleted: boolean;
+  id: string;
+}
 
 export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleNewTask(task: Task) {
+    setTasks((prevState) => [...prevState, task]);
+  }
+
+  function handleTaskRemove(taskToRemove: Task) {
+    setTasks((prevState) =>
+      prevState.filter((task) => task.id !== taskToRemove.id)
+    );
+  }
+
+  function handleCompleteTask(isChecked: boolean, taskToUpdate: Task) {
+    const tasksUpdated = tasks.map((task) =>
+      task.id === taskToUpdate.id ? { ...task, isCompleted: isChecked } : task
+    );
+
+    setTasks(tasksUpdated)
+  }
+
   return (
     <View style={styles.homeContainer}>
       <Header />
 
-      <CreateTask />
-      <SummaryTasks />
+      <CreateTask onCreateTask={handleNewTask} />
+      <SummaryTasks tasks={tasks} />
 
-      <CardTask />
+      <FlatList
+        style={styles.flatList}
+        data={tasks}
+        keyExtractor={(task) => task.id}
+        renderItem={(task) => (
+          <CardTask
+            name={task.item.name}
+            onRemove={() => handleTaskRemove(task.item)}
+            isCompleted={task.item.isCompleted}
+            onCompleteTask={(isCompleted) =>
+              handleCompleteTask(isCompleted, task.item)
+            }
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => <EmptyTask />}
+      />
     </View>
   );
 }
